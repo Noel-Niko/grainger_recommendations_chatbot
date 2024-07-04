8c215839a80dd432ad955307105915e875fd4594
Noel-Niko <83922762+Noel-Niko@users.noreply.github.com>
2024-07-04 14:04:32 -0400

    Implementing async search for reviews and images

--- modules/main.py
+++ modules/main.py
@@ -58,16 +58,20 @@ class StreamlitInterface:
             logging.info(f"Products: {products}")
 
             center_col.subheader("Response:")
+            time_to_initially_respond = time.time() - start_time
+            center_col.write(f"Time taken to generate response: {time_to_initially_respond}")
             center_col.write(message)
-            center_col.write(f"Time taken: {time_taken}")
+
+            center_col.write(f"Time taken to generate customer attributes: {time_taken}")
             center_col.write(f"Customer attributes identified: {customer_attributes_retrieved}")
 
-            await self.display_grainger_images(col3, products)
-
+            await asyncio.gather(
+                self.display_grainger_images(col3, products),
+                self.display_reviews(products)
+            )
             total_time = time.time() - start_time
             center_col.write(f"Total time to answer: {total_time}")
 
-            await self.display_reviews(products)
 
     def process_chat_question(self, question, clear_history=False):
         message, response_json, total_time, customer_attributes_retrieved = process_chat_question_with_customer_attribute_identifier(
@@ -129,123 +133,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# import asyncio
-# import io
-# import logging
-# import time
-# import os
-# import base64
-# import tempfile
-# from PIL import Image
-# import streamlit as st
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
-#
-# from web_extraction_tools.product_reviews.call_selenium_for_review_async import async_navigate_to_reviews_selenium
-# from image_utils.grainger_image_util import main as generate_grainger_thumbnails, get_images
-# from vector_index import Document as vectorIndexDocument
-# from vector_index.chat_processor import process_chat_question_with_customer_attribute_identifier
-#
-# class StreamlitInterface:
-#     def __init__(self, index_document, LLM, bedrock_titan_embeddings, data_frame_singleton):
-#         self.document = index_document
-#         self.llm = LLM
-#         self.bedrock_embeddings = bedrock_titan_embeddings
-#         self.chat_history = []
-#         self.df = data_frame_singleton
-#         self.options = Options()
-#         self.options.add_argument("--headless")
-#         self.options.add_argument("--disable-gpu")
-#         self.service = Service(ChromeDriverManager().install())
-#         self.driver = webdriver.Chrome(service=self.service, options=self.options)
-#
-#     def run(self):
-#         st.set_page_config(layout="wide")
-#         st.title("Grainger Recommendations Chatbot")
-#
-#         main_column, side_column = st.columns([2, 1])
-#
-#         with main_column:
-#             asyncio.run(self.ask_question(main_column, side_column))
-#
-#     async def ask_question(self, center_col, col3):
-#         question = st.text_input("Enter your question:", value="", placeholder="", key="unique_key_for_question")
-#         start_time = time.time()
-#         if question:
-#             message, response_json, time_taken, customer_attributes_retrieved = self.process_chat_question(
-#                 question=question, clear_history=False)
-#
-#             products = response_json.get('products', [])
-#             logging.info(f"Products: {products}")
-#
-#             center_col.subheader("Response:")
-#             center_col.write(message)
-#             center_col.write(f"Time taken: {time_taken}")
-#             center_col.write(f"Customer attributes identified: {customer_attributes_retrieved}")
-#
-#             await self.display_grainger_images(col3, products)
-#
-#             total_time = time.time() - start_time
-#             center_col.write(f"Total time to answer: {total_time}")
-#
-#             await self.display_reviews(products)
-#
-#     def process_chat_question(self, question, clear_history=False):
-#         message, response_json, total_time, customer_attributes_retrieved = process_chat_question_with_customer_attribute_identifier(
-#             question,
-#             self.document,
-#             self.llm,
-#             self.chat_history,
-#             clear_history)
-#         return message, response_json, total_time, customer_attributes_retrieved
-#
-#     async def display_grainger_images(self, col3, products):
-#         start_time_col3 = time.time()
-#
-#         recommendations_list = [f"{product['product']}, {product['code']}" for product in products]
-#
-#         image_data, total_image_time = await get_images(recommendations_list, self.df)
-#
-#         for image_info in image_data:
-#             try:
-#                 img = Image.open(io.BytesIO(image_info["Image Data"]))
-#                 col3.image(img, caption=f"Grainger Product Image ({image_info['Code']})", use_column_width=True)
-#             except Exception as e:
-#                 logging.error(f"Error displaying image: {e}")
-#
-#         total_time_col3 = time.time() - start_time_col3
-#         col3.write(f"Time fetching images: {total_time_col3}")
-#
-#     async def display_reviews(self, products):
-#         start_time_col1 = time.time()
-#         logging.info("Entering display_reviews method")
-#         recommendations_list = [f"{product['product']}, {product['code']}" for product in products]
-#
-#         reviews_data = await async_navigate_to_reviews_selenium(recommendations_list[0], self.driver)
-#         if reviews_data:
-#             st.subheader('Extracted Reviews:')
-#             st.write(f"Average Star Rating: {reviews_data['Average Star Rating']}")
-#             st.write(f"Average Recommendation Percent: {reviews_data['Average Recommendation Percent']}")
-#             st.write("Review Texts:")
-#             for idx, review_text in enumerate(reviews_data['Review Texts'], start=1):
-#                 st.write(f"\nReview {idx}: {review_text}")
-#         else:
-#             st.write("No reviews found for the given Product ID(s).")
-#
-#         end_time_col1 = time.time()
-#         total_time_col1 = end_time_col1 - start_time_col1
-#         st.write(f"Time searching for reviews: {total_time_col1}")
-#
-#         return reviews_data
-#
-#
-# if __name__ == "__main__":
-#     document, llm, bedrock_embeddings, df = vectorIndexDocument.get_instance()
-#     interface = StreamlitInterface(index_document=document, LLM=llm, bedrock_titan_embeddings=bedrock_embeddings,
-#                                    data_frame_singleton=df)
-#     interface.run()

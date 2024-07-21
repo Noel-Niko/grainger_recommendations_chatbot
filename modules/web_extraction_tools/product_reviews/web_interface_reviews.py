2f7ec750d1332fe76098cbfb11ccea6746bc729a
Noel-Niko <nnosse@wgu.edu>
2024-07-21 12:52:11 -0400

    Linting with Ruff

--- modules/web_extraction_tools/product_reviews/web_interface_reviews.py
+++ modules/web_extraction_tools/product_reviews/web_interface_reviews.py
@@ -21,7 +21,7 @@ class ReviewExtractor:
         self.driver.quit()
 
     def extract_reviews_for_product(self, product_id):
-        reviews_data = navigate_to_reviews_selenium(product_id, self.driver)
+        reviews_data = navigate_to_reviews_selenium(product_id)
         return reviews_data
 
 def main():
@@ -31,19 +31,18 @@ def main():
 
     product_id = st.text_input('Enter Product ID (e.g., 1VCE8):')
 
-    if st.button('Extract Reviews'):
-        if product_id:
-            st.write(f"Extracting reviews for Product ID: {product_id}...")
-            reviews_data = extractor.extract_reviews_for_product(product_id)
-            if reviews_data:
-                st.subheader('Extracted Reviews:')
-                for idx, review in enumerate(reviews_data, start=1):
-                    st.write(f"\nReview {idx}:")
-                    st.write(f"Star Rating: {review['Star Rating']}")
-                    st.write(f"Rating Text: {review['Rating Text']}")
-                    st.write(f"Review Text: {review['Review Text']}")
-            else:
-                st.write("No reviews found for the given Product ID.")
+    if st.button('Extract Reviews') and product_id:
+        st.write(f"Extracting reviews for Product ID: {product_id}...")
+        reviews_data = extractor.extract_reviews_for_product(product_id)
+        if reviews_data:
+            st.subheader('Extracted Reviews:')
+            for idx, review in enumerate(reviews_data, start=1):
+                st.write(f"\nReview {idx}:")
+                st.write(f"Star Rating: {review['Star Rating']}")
+                st.write(f"Rating Text: {review['Rating Text']}")
+                st.write(f"Review Text: {review['Review Text']}")
+        else:
+            st.write("No reviews found for the given Product ID.")
 
 if __name__ == '__main__':
     main()

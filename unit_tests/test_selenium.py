accb7d1aab1e1caaf38c9df404bca854a73e3cca
Noel-Niko <nnosse@wgu.edu>
2024-07-24 21:46:52 -0400

    Added pip installs required for test to ci.yml

--- unit_tests/test_selenium.py
+++ unit_tests/test_selenium.py
@@ -1,11 +1,14 @@
 import os
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 # Get the Chrome binary path from environment variable
 chrome_binary_path = os.getenv("CHROME_BINARY_PATH")
+
+if not chrome_binary_path:
+    raise ValueError("The CHROME_BINARY_PATH environment variable is not set or is invalid.")
+
 options = Options()
 options.binary_location = chrome_binary_path
 
